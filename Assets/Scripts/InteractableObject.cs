83420df86edf37d0285435c3795a4db03b604ec8
LAPTOP-M7VGAAAU\Adebi Family <iadebi@andrew.cmu.edu>
2017-07-27 16:23:40 -0400

    Added Interactivity

---
+++ Assets/Scripts/InteractableObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObject : MonoBehaviour {
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // On mouse down
+    private void OnMouseDown()
+    {
+        print("Wassup");
+    }
+}
